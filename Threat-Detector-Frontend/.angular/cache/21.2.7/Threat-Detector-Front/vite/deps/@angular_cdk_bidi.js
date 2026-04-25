774d4f307464e599944856b2413b72b55c955764
Salmabentisse <bentissesalma18@gmail.com>
2026-04-25 14:05:41 +0200

    Rename folders to Threat-Detector-Frontend and Threat-Detector-Backend

---
+++ Threat-Detector-Frontend/.angular/cache/21.2.7/Threat-Detector-Front/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,13 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-LFROMONJ.js";
+import "./chunk-JMYJSAJO.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ Threat-Detector-Frontend/.angular/cache/21.2.7/Threat-Detector-Front/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,48 @@
+import {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-QWAIH2ZX.js";
+import "./chunk-SJE5W2WK.js";
+import {
+  Dir
+} from "./chunk-LFROMONJ.js";
+import "./chunk-UYQCR537.js";
+import "./chunk-TSKWCPWT.js";
+import "./chunk-JMYJSAJO.js";
+export {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
